b02d14be4beabe7f1eb7b0bc84ff43a49e7f4ca6
vishalsharma2502 <33055693+vishalsharma2502@users.noreply.github.com>
2019-01-08 22:20:07 +0530

    Add files via upload

---
+++ 5 Pointers/3. Reference.cpp
@@ -0,0 +1,7 @@
+Reference
+• Reference is a Alias of variable
+• It must be initialised when declared
+• It doesn’t take any memory
+• It cannot be modified to refer other variable
+• Syntax for reference declaration is
+• Int &y=x;
