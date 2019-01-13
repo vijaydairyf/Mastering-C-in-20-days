b4fffb56e03ee5606064a1ade6c53ca0e8814af9
vishalsharma2502 <33055693+vishalsharma2502@users.noreply.github.com>
2019-01-13 09:18:11 +0530

    Add files via upload

---
+++ 6 Functions/4.Maximum Number.cpp
@@ -0,0 +1,21 @@
+//Example of function to find Maximum of 3 number
+#include<iostream>
+using namespace std;
+int maxim(int a,int b,int c)
+{
+if(a>b && a>c)
+return a;
+else if(b>c)
+return b;
+else
+return c;
+}
+int main()
+{
+int a,b,c,d;
+cout<<"Enter three no.s ";
+cin>>a>>b>>c;
+d=maxim(a,b,c);
+cout<<"Maximum is "<<d<<endl;
+return 0;
+}
